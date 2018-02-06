29a852f1490b92cb2960e929aaa04eb4f2ebf02e
vywu <ywu654@uwo.ca>
2018-02-06 14:34:14 -0500

    added schedule

--- js/fade.js
+++ js/fade.js
@@ -1,31 +1,35 @@
-function fadeNav(){  
-  var offset = getScrollXY();
-  //if y offset is greater than 0, set opacity to desired value, otherwise set to 1
-  offset[1] > 0 ? setNavOpacity(0.5) : setNavOpacity(1.0); 
-}
 
-function setNavOpacity(newOpacity){
-  var navBar = document.getElementById("header_bar");
-  navBar.style.opacity = newOpacity;
-}
+//  window.onscroll = fadeNav;
 
-function getScrollXY() {
-  var scrOfX = 0, scrOfY = 0;
-if( typeof( window.pageYOffset ) == 'number' ) {
-  //Netscape compliant
-  scrOfY = window.pageYOffset;
-  scrOfX = window.pageXOffset;
-} else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
-  //DOM compliant
-  scrOfY = document.body.scrollTop;
-  scrOfX = document.body.scrollLeft;
-} else if( document.documentElement && ( document.documentElement.scrollLeft ||     
-  document.documentElement.scrollTop ) ) {
-  //IE6 standards compliant mode
-  scrOfY = document.documentElement.scrollTop;
-  scrOfX = document.documentElement.scrollLeft;
-}
+// function fadeNav(){  
+//   var offset = getScrollXY();
+//   //if y offset is greater than 0, set opacity to desired value, otherwise set to 1
+//   offset[1] > 0 ? setNavOpacity(0.5) : setNavOpacity(1.0); 
+// }
 
-  return [ scrOfX, scrOfY ];
-}
+// function setNavOpacity(newOpacity){
+//   console.log(newOpacity);
+//   var navBar = document.getElementById("mainNav");
+//   newOpacity==0.5? navBar.style.backgroundColor = "transparent":navBar.style.backgroundColor="#f3b5b5";
+ 
+// }
 
+// function getScrollXY() {
+//   var scrOfX = 0, scrOfY = 0;
+// if( typeof( window.pageYOffset ) == 'number' ) {
+//   //Netscape compliant
+//   scrOfY = window.pageYOffset;
+//   scrOfX = window.pageXOffset;
+// } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+//   //DOM compliant
+//   scrOfY = document.body.scrollTop;
+//   scrOfX = document.body.scrollLeft;
+// } else if( document.documentElement && ( document.documentElement.scrollLeft ||     
+//   document.documentElement.scrollTop ) ) {
+//   //IE6 standards compliant mode
+//   scrOfY = document.documentElement.scrollTop;
+//   scrOfX = document.documentElement.scrollLeft;
+// }
+
+//   return [ scrOfX, scrOfY ];
+// }
